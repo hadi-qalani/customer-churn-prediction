a8e9c342d5d7d8886562e83fbd80562ce15a3145
hadi-qalani <  GitHub>
2026-07-23 12:51:45 +0330

    feat: integrate preprocessing and model training using static configuration

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import recall_score, precision_score, accuracy_score, f1_score
+from sklearn.linear_model import LogisticRegression
 
 from src.data.dataset import prepare_dataset
+from src.evaluation.evaluator import Evaluator
+from src.evaluation.sklearn import SklearnEvaluationStrategy
 from src.features.engineering import IdentityFeatureEngineer
 from src.features.pipeline import FeatureEngineeringPipeline
 from src.ml_preprocessing.preprocessor import preprocess_dataset
@@ -63,9 +66,10 @@ PREPROCESSING_CONFIG = {
     },
 }
 
-
 def main() -> None:
-    df = prepare_dataset("WA_Fn-UseC_-Telco-Customer-Churn.csv", required_columns=REQUIRED_COLUMNS)
+    df = prepare_dataset(
+        "WA_Fn-UseC_-Telco-Customer-Churn.csv", required_columns=REQUIRED_COLUMNS
+    )
 
     samp = IdentityFeatureEngineer()
     feature_eng = FeatureEngineeringPipeline(engineers=[samp])
@@ -74,14 +78,19 @@ def main() -> None:
     splitted = split_dataset(df, "Churn")
     processed_data = preprocess_dataset(dataset=splitted, config=PREPROCESSING_CONFIG)
 
-    model = DecisionTreeClassifier(max_depth=30)
+    model = LogisticRegression(class_weight="balanced")
 
-    strategy = SklearnTrainingStrategy(model)
-    trainer = Trainer(strategy)
-    result = trainer.train(data=processed_data)
+    train_strategy = SklearnTrainingStrategy(model)
+    trainer = Trainer(train_strategy)
+    train_result = trainer.train(data=processed_data)
+
+    eval_strategy = SklearnEvaluationStrategy([recall_score,precision_score, accuracy_score, f1_score])
+    evaluator = Evaluator(strategy=eval_strategy)
+    eval_result = evaluator.evaluate(model=model, processed_data=processed_data)
 
     print(df.head())
-    print(result)
+    print(train_result)
+    print(eval_result)
 
 
 if __name__ == "__main__":
